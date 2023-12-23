75a85b08b6a31c845c9c2700c3bffde0fbcda184
Yashwanth A <123303508+yashwanth2706@users.noreply.github.com>
2023-12-23 23:55:25 +0530

    Add files via upload
    need to add sound and font folders

---
+++ position.cpp
@@ -0,0 +1,6 @@
+#include "position.h"
+
+Position::Position(int row, int column) {
+    this->row = row; //-> symbol assigns the row parameter to the row attribute of the current obj
+    this->column = column;
+}
